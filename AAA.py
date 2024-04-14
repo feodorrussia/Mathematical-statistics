fe630d1fe87a264c49f0c6fe1c4e83f2d3942b54
feodorrussia <fvdelows@mail.ru>
2024-04-14 14:55:26 +0300

    Upload Lab 4

---
+++ AAA.py
@@ -0,0 +1,6 @@
+a = 2.
+x = float(input())
+while x != -1:
+    print(abs(a - x) / a)
+
+    x = float(input())
---
+++ Lab_4/main.py
@@ -0,0 +1,5 @@
+from task import Task4
+
+if __name__ == "__main__":
+    task = Task4()
+    task.run()
